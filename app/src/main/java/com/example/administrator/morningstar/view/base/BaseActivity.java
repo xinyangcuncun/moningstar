cb8d510eaa7d959a291e9d76b8628c17dee716bb
renchangcun@kaike.la <renchangcun@kaike.la>
2017-03-23 17:16:07 +0800

    测试提交

--- app/src/main/java/com/example/administrator/morningstar/view/base/BaseActivity.java
+++ app/src/main/java/com/example/administrator/morningstar/view/base/BaseActivity.java
@@ -29,6 +29,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private TextView mTitleView;
     private TextView haha;
+    private TextView haha_master;
 
 
     private int toolbarId;
